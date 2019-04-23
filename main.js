4ebbce12ea3e7907d8d12134bd21c40e3b7e0d4a
Lucaspojo <lucaspaureau@gmail.com>
2019-04-23 23:21:39 +0200

    Update

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1300,
+    height: 850,
     webPreferences: {
       nodeIntegration: true
     }
